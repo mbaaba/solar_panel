1459c53c008edf97ee6db657a8c72084a42e2904
Wolfgang Egger <w.egger@framlin.com>
2019-07-20 15:58:02 +0200

    LoRa FeatherWing 868Mhz Sender
    Changing to a M4 Express and a LoRa FeatherWing and to 868 MHz

--- src/LoRaSender/main.py
+++ src/LoRaSender/main.py
@@ -1,15 +1,21 @@
-print("holldrio LoRa Sender")
+print("holldrio M4 LoRa Sender")
 from time import sleep
+
 import board
 import busio
 import digitalio
+
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-cs = digitalio.DigitalInOut(board.RFM9X_CS)
-reset = digitalio.DigitalInOut(board.RFM9X_RST)
+cs = digitalio.DigitalInOut(board.D5)
+reset = digitalio.DigitalInOut(board.D6)
+
+# cs = digitalio.DigitalInOut(board.RFM9X_CS)
+# reset = digitalio.DigitalInOut(board.RFM9X_RST)
+
 
 import adafruit_rfm9x
-rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 433.0)
+rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 868.0)
 
 ctr = 0;
 
@@ -17,4 +23,5 @@ while True:
     msg = "send message #{}".format(ctr)
     rfm9x.send(msg)
     ctr += 1
-    sleep(1)
+    sleep(10)
+    print('round {}'.format(ctr))
